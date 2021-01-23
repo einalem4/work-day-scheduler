6569d94680e782d08cf1389cf61303876fc043ac
einalem4 <melanie@codehangar.io>
2021-01-22 17:41:46 -0700

    set current time

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,25 @@
-   // set to current day
-   var currentDay = moment().format("dddd, MMMM Do");
-   $("#currentDay").text(currentDay);
+
+
+// set to current day
+var currentDay = moment().format("dddd, MMMM Do");
+$("#currentDay").text(currentDay);
+
+var currentTime = parseInt(moment().format("HH"));
+
+$(document.body).ready(function () {
+    $('textarea').each(function (i) {
+        parseInt(this.id);
+        if (this.id > currentTime) {
+            $(this.id).addClass("textarea bg-danger");
+        } else if (this.id < currentTime) {
+            $(this.id).addClass("textarea bg-warning");
+        }
+        else if (this.id === currentTime) {
+            $(this.id).addClass("textarea bg-success");
+        }
+        console.log(currentTime);
+        console.log(this.id);
+    })
+});
+
+
