0e1e79187b6021548bd96982a91e64fc62b7a802
einalem4 <melanie@codehangar.io>
2021-01-23 10:55:11 -0700

    set/get localstorage

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,42 @@
 
-
-// set to current day
+//current day
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
 
-// var currentTime = parseInt(moment().format("HH",));
+//current hour
+var currentTime = parseInt(moment().format("HH",));
 
-var currentTime = 12;
 
+// loop through time and set colors according to past, present, future time
 $(document.body).ready(function () {
     $('textarea').each(function (i) {
         var thisID = parseInt(this.id);
         if (thisID > currentTime) {
             $("#" + thisID).addClass("textarea bg-danger");
-            console.log("DANGER", thisID);
         } else if (thisID < currentTime) {
             $("#" + thisID).addClass("textarea bg-secondary");
-            console.log("SECONDARY", thisID);
         }
         else if (thisID === currentTime) {
             $("#" + thisID).addClass("textarea bg-success");
-            console.log("SUCCESSS", thisID);
         }
-        console.log(currentTime);
-        console.log(thisID);
     })
-});
 
+
+    //get saved items to localstorage
+    function savedText() {
+        $("textarea").each(function () {
+            var input = $(this).attr("id");
+            $(this).val(localStorage.getItem(input));
+        });
+    };
+
+    //save items to localstorage
+    $(".saveBtn").click(function () {
+        var schedule = $(this).siblings("textarea").val();
+        var textLocation = $(this).siblings("textarea").attr("id");
+        localStorage.setItem(textLocation, schedule);
+    });
+
+    savedText();
+});
 
